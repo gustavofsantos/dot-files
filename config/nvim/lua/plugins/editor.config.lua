6efe505d849e9b01127b5b575a8b3d00e616aabb
Gustavo Santos <gustavogustav54@gmail.com>
2025-04-07 13:57:38 -0300

    feat(nvim): move keymaps and lazy load plugins

--- config/nvim/lua/plugins/editor.config.lua
+++ config/nvim/lua/plugins/editor.config.lua
@@ -5,15 +5,10 @@ return {
   {
     "mbbill/undotree",
     cmd = "UndotreeToggle",
-    keys = {
-      { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "toggle undo tree" },
-    }
   },
   {
     "AndrewRadev/switch.vim",
-    keys = {
-      { "<leader>ss", "<cmd>Switch<CR>" }
-    },
+    cmd = { "Switch" },
     config = function()
       vim.cmd([[let g:switch_custom_definitions =
           \ [
@@ -79,124 +74,6 @@ return {
           hex_color = hipatterns.gen_highlighter.hex_color(),
         },
       })
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-    enabled = false,
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-      "nvim-neotest/nvim-nio",
-      "mfussenegger/nvim-dap-python",
-    },
-    ft = { "python" },
-    event = "VeryLazy",
-    config = function()
-      require("dapui").setup({
-        controls = {
-          element = "repl",
-          enabled = true,
-          icons = {
-            disconnect = "",
-            pause = "",
-            play = "",
-            run_last = "",
-            step_back = "",
-            step_into = "",
-            step_out = "",
-            step_over = "",
-            terminate = "",
-          },
-        },
-        element_mappings = {},
-        expand_lines = true,
-        floating = {
-          border = "single",
-          mappings = {
-            close = { "q", "<Esc>" },
-          },
-        },
-        force_buffers = true,
-        icons = {
-          collapsed = "",
-          current_frame = "",
-          expanded = "",
-        },
-        layouts = {
-          {
-            elements = {
-              {
-                id = "scopes",
-                size = 0.5,
-              },
-              {
-                id = "breakpoints",
-                size = 0.25,
-              },
-              {
-                id = "stacks",
-                size = 0.25,
-              },
-            },
-            position = "left",
-            size = 40,
-          },
-          {
-            elements = {
-              {
-                id = "repl",
-                size = 0.5,
-              },
-              {
-                id = "console",
-                size = 0.5,
-              },
-            },
-            position = "bottom",
-            size = 10,
-          },
-        },
-        mappings = {
-          edit = "e",
-          expand = { "<CR>", "<2-LeftMouse>" },
-          open = "o",
-          remove = "d",
-          repl = "r",
-          toggle = "t",
-        },
-        render = {
-          indent = 1,
-          max_value_lines = 100,
-        },
-      })
-      local dap, dapui = require("dap"), require("dapui")
-
-      vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DiagnosticError", linehl = "", numhl = "" })
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-
-      -- see: https://github.com/mfussenegger/nvim-dap-python?tab=readme-ov-file#debugpy
-      require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
-
-      vim.keymap.set("n", "<leader>cdu", dapui.toggle, { desc = "toggle dap ui" })
-      vim.keymap.set("n", "<F5>", dap.continue, { desc = "continue" })
-      vim.keymap.set("n", "<F8>", dap.toggle_breakpoint, { desc = "toggle breakpoint" })
-      vim.keymap.set("n", "<F10>", dap.step_over, { desc = "step over" })
-      vim.keymap.set("n", "<F11>", dap.step_into, { desc = "step into" })
-      vim.keymap.set("n", "<F12>", dap.step_out, { desc = "step out" })
-      vim.keymap.set("n", "<leader>cdh", function() require("dap.ui.widgets").hover() end, { desc = "hover" })
-      vim.keymap.set("n", "<leader>cdp", function() require("dap.ui.widgets").preview() end, { desc = "preview" })
     end,
   },
   {
--- config/nvim/plugin/keymaps.lua
+++ config/nvim/plugin/keymaps.lua
@@ -15,10 +15,19 @@ set(t, "<Esc>", "<c-\\><c-n>")
 set(n, "<leader><leader>", "<c-^>", { desc = "Switch buffer" })
 set(n, "]c", "<cmd>cnext<cr>", { desc = "quickfix next" })
 set(n, "[c", "<cmd>cprevious<cr>", { desc = "quickfix previous" })
-
+set(n, "<leader>u", "<cmd>UndotreeToggle<cr>", { desc = "Undo tree" })
 set(v, "<leader>b",
   ':<C-U>!git blame <C-R>=expand("%:p") <CR> | sed -n <C-R>=line("\'<") <CR>,<C-R>=line("\'>") <CR>p <CR>')
 
+set(n, "<leader>o", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true, desc = "Find files" })
+set(n, "<leader>b", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true, desc = "Find buffer" })
+set(n, "<leader>l", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true, desc = "Live grep" })
+set(n, "<leader>f", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { noremap = true, silent = true, desc = "Buffer fuzzy find" })
+set(v, "<F3>", '"zy:Telescope grep_string default_text=<C-r>z<cr>', { noremap = true, silent = true, desc = "Find Selected" })
+set(n, "<F3>", "<cmd>Telescope grep_string<cr>", { noremap = true, silent = true, desc = "Find Word" })
+
+set(n , "<leader>?", "<cmd>AvanteToggle<CR>", { noremap = true, silent = true, desc = "Open Avante" })
+set(n, "<leader>ss", "<cmd>Switch<CR>", { noremap = true, silent = true, desc = "Switch" })
 set(n, "<leader>ws", "<cmd>vsplit<cr>", { desc = "split window", noremap = true, silent = true })
 set(n, "<leader>wS", "<cmd>split<cr>", { desc = "split window down", noremap = true, silent = true })
 
@@ -30,10 +39,4 @@ set(n, "<leader>;g", "<cmd>GlobalBookmarks<CR>", { desc = "All bookmarks" })
 
 set(n, "-", "<cmd>Oil<CR>", { noremap = true, silent = true, desc = "File explorer" })
 
-set(n, "<leader>o", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true, desc = "Find files" })
-set(n, "<leader>b", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true, desc = "Find buffer" })
-set(n, "<leader>l", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true, desc = "Live grep" })
-set(v, "<F3>", '"zy:Telescope grep_string default_text=<C-r>z<cr>', { desc = "Find Selected" })
-set(n, "<F3>", "<cmd>Telescope grep_string<cr>", { desc = "Find Word" })
 
-set(n , "<leader>?", "<cmd>AvanteToggle<CR>", { desc = "Open Avante" })
