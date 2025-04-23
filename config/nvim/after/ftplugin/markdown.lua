a93d85598f844c222e5b0b22deacc23f0061c584
Gustavo Santos <gustavogustav54@gmail.com>
2025-04-23 11:39:20 -0300

    fix(nvim): weird markdown mapping

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -3,7 +3,5 @@ vim.cmd([[setlocal nonumber]])
 vim.cmd([[setlocal textwidth=80]])
 vim.cmd([[setlocal nolist]])
 
-vim.api.nvim_buf_set_keymap(0, "n", "h", "gh", { silent = true, noremap = true })
 vim.api.nvim_buf_set_keymap(0, "n", "j", "gj", { silent = true, noremap = true })
 vim.api.nvim_buf_set_keymap(0, "n", "k", "gk", { silent = true, noremap = true })
-vim.api.nvim_buf_set_keymap(0, "n", "l", "gl", { silent = true, noremap = true })
