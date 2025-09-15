753069d7ee8037244571f179b0608c64f33784f5
Gustavo Santos <gustavogustav54@gmail.com>
2025-09-15 14:00:42 -0300

    feat(wezterm): configure term on mac

---
+++ config/wezterm/colors/Nordic.lua
@@ -0,0 +1,54 @@
+return {
+    foreground    = "#D8DEE9",
+    background    = "#242933",
+    cursor_bg     = "#D8DEE9",
+    cursor_border = "#D8DEE9",
+    cursor_fg     = "#242933",
+    selection_fg  = "#D8DEE9",
+    selection_bg  = "#2E3440",
+    split         = "#3B4252",
+
+    ansi          = { "#191D24", "#BF616A", "#A3BE8C", "#EBCB8B", "#81A1C1", "#B48EAD", "#8FBCBB", "#D8DEE9" },
+    brights       = { "#3B4252", "#D06F79", "#B1D196", "#F0D399", "#88C0D0", "#C895BF", "#93CCDC", "#E5E9F0" },
+
+    indexed = {
+        [16] = "#ffa066",
+        [17] = "#ff5d62",
+        [18] = "#415570",
+    },
+
+    tab_bar       = {
+        background              = "#191D24",
+        inactive_tab_edge       = "#191D24",
+        inactive_tab_edge_hover = "#3B4252",
+
+        active_tab              = {
+            bg_color = "#2E3440",
+            fg_color = "#8FBCBB",
+            intensity = "Normal",
+            italic = false,
+            strikethrough = false,
+            underline = "None",
+        },
+
+        inactive_tab = {
+            bg_color = "#191D24",
+            fg_color = "#D8DEE9",
+        },
+
+        inactive_tab_hover = {
+            bg_color = "#191D24",
+            fg_color = "#D8DEE9",
+        },
+
+        new_tab = {
+            bg_color = "#191D24",
+            fg_color = "#D8DEE9",
+        },
+
+        new_tab_hover = {
+            bg_color = "#191D24",
+            fg_color = "#D8DEE9"
+        }
+    }
+}
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,16 +8,9 @@ require("statusline").setup()
 
 config.automatically_reload_config = false
 config.force_reverse_video_cursor = true
-config.colors = require("colors.Kanagawa")
+config.colors = require("colors.Nordic")
 
 config.font = wezterm.font("MonoLisa Nerd Font")
--- config.font = wezterm.font_with_fallback({
---     { family = "OperatorMono Nerd Font", weight = "Regular" },
---     "JetBrains Mono",
---     "Noto Color Emoji",
---     "Symbols Nerd Font Mono",
--- })
--- config.font = wezterm.font("BerkeleyMono Nerd Font")
 config.font_size = 14
 config.max_fps = 120
 config.harfbuzz_features = { "calt=1", "clig=1", "liga=0" }
@@ -38,11 +31,11 @@ config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_max_width = 999999
 
-for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
-    if gpu.backend == "Vulkan" and (gpu.device_type == "IntegratedGpu" or gpu.device_type == "DiscreteGpu") then
-        config.webgpu_preferred_adapter = gpu
-        config.front_end = "WebGpu"
-    end
-end
+-- for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
+--     if gpu.backend == "Vulkan" and (gpu.device_type == "IntegratedGpu" or gpu.device_type == "DiscreteGpu") then
+--         config.webgpu_preferred_adapter = gpu
+--         config.front_end = "WebGpu"
+--     end
+-- end
 
 return config
