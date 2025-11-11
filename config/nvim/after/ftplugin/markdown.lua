83d41c5426acc7bf9287ca4fce394234eb7fc500
Gustavo Santos <gustavogustav54@gmail.com>
2025-11-11 14:37:11 -0300

    chore: comment conceal

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -2,7 +2,7 @@
 vim.cmd([[setlocal nonumber]])
 -- vim.cmd([[setlocal textwidth=80]])
 -- vim.cmd([[setlocal nolist]])
-vim.opt_local.conceallevel = 2
+-- vim.opt_local.conceallevel = 2
 vim.opt_local.wrap = true
 
 vim.api.nvim_buf_set_keymap(0, "n", "j", "gj", { silent = true, noremap = true })
