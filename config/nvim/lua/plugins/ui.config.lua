b45884de292e901319f366f9333f843e13391f55
Gustavo Santos <gustavogustav54@gmail.com>
2025-11-26 18:38:13 -0300

    feat(nvim): config lualine

--- config/nvim/lua/plugins/ui.config.lua
+++ config/nvim/lua/plugins/ui.config.lua
@@ -2,7 +2,24 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     config = function()
-      require('lualine').setup()
+      require('lualine').setup({
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = {},
+          lualine_c = { { 'filename', path = 1 } },
+          lualine_x = { 'lsp_status', 'diagnostics' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { { 'filename', path = 1 } },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {}
+        },
+      })
     end
   },
   {
