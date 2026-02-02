eead3cb22476a40d4aa0fc3de76183f942014b1c
Gustavo Santos <gustavogustav54@gmail.com>
2026-02-02 09:52:09 -0300

    feat(nvim): add lualine back

--- config/nvim/lua/plugins/ui.config.lua
+++ config/nvim/lua/plugins/ui.config.lua
@@ -15,6 +15,36 @@ return {
     end,
   },
   {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { 'overseer' },
+        lualine_y = { 'location' },
+        lualine_z = {}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'overseer', 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      extensions = {
+        'fugitive',
+        'lazy',
+        'oil',
+        'overseer',
+        'quickfix',
+        'toggleterm'
+      }
+    }
+  },
+  {
     'stevearc/quicker.nvim',
     event = "FileType qf",
     ---@module "quicker"
