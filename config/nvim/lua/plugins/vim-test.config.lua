258ee7a4f790c1ff9cc0f650424ebeaa63d2cfa2
Gustavo Santos <gustavogustav54@gmail.com>
2026-06-03 18:14:09 -0300

    chore: custom vim-test transformer to run custom scripts

--- config/nvim/lua/plugins/vim-test.config.lua
+++ config/nvim/lua/plugins/vim-test.config.lua
@@ -1,14 +1,33 @@
 return {
   "vim-test/vim-test",
-  config = function ()
+  config = function()
     vim.cmd([[let test#strategy = "vtr"]])
+
+    local function find_monorepo_root()
+      -- Look for common monorepo root markers
+      local root_markers = { ".git" }
+      local root = vim.fs.root(0, root_markers)
+
+      return root or vim.fn.getcwd()
+    end
+
+    -- 2. Define the Command Transformation
+    _G.custom_testf = function(cmd)
+      local repo_root = vim.fn.getcwd()
+      local relative_file = vim.fn.expand("%")
+
+      return "source ~/.zhelpers && testf " .. repo_root .. " " .. relative_file
+    end
+
     -- vim.cmd([[let test#javascript#playwright#options = "--headed --retries 0 --workers 1"]])
     -- vim.cmd([[let test#clojure#runner = "leintest"]])
     -- vim.cmd([[let test#clojure#leintest#executable = "lein with-profile test midje"]])
     -- vim.cmd([[let test#clojure#leintest#options = ""]])
 
+    vim.g["test#custom_transformations"] = { custom_testf = _G.custom_testf }
+    vim.g["test#transformation"] = "custom_testf"
+
     vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>", { desc = "Test file", noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>tf", "<cmd>TestNearest<CR>", { desc = "Test nearest", noremap = true, silent = true })
     vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<CR>", { desc = "Visit last test", noremap = true, silent = true })
     vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>", { desc = "Test last file", noremap = true, silent = true })
   end
