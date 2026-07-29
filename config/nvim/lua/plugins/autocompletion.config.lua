04459179b69f7af782a001f400cd61559537b09f
Gustavo Santos <gustavogustav54@gmail.com>
2026-07-29 10:49:19 -0300

    chore: organize autocomplete order

--- config/nvim/lua/plugins/autocompletion.config.lua
+++ config/nvim/lua/plugins/autocompletion.config.lua
@@ -70,11 +70,11 @@ return {
       },
       sources = {
         { name = "luasnip",  keyword_length = 2, max_item_count = 2 },
-        { name = "buffer",   keyword_length = 3, max_item_count = 5 },
         { name = "nvim_lsp" },
+        { name = "buffer",   keyword_length = 3, max_item_count = 5 },
+        { name = "path",     keyword_length = 5, max_item_count = 5 },
         { name = 'emoji' },
         { name = "nvim_lua", keyword_length = 3, max_item_count = 5 },
-        { name = "path",     keyword_length = 5, max_item_count = 5 },
       },
     })
 
