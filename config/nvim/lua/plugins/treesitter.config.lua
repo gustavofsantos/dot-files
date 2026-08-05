3579fd3a2dbbca59fc0dce796360bc55bd4b0662
Gustavo Santos <gustavogustav54@gmail.com>
2026-08-05 14:18:21 -0300

    chore(nvim): update config

--- config/nvim/lua/plugins/treesitter.config.lua
+++ config/nvim/lua/plugins/treesitter.config.lua
@@ -1,5 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      event = "BufReadPre",
+      opts = {
+        max_lines = 3,    -- How many lines the sticky context can take up
+        trim_scope = "outer", -- Collapse context lines if they exceed max_lines
+      }
+    }
+  },
   build = ":TSUpdate",
   branch = vim.fn.has("nvim-0.12") == 1 and "main" or "master",
   lazy = false,
