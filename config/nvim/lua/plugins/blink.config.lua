1df0869281680d608b9300d2b7df04c158b41398
Gustavo Santos <gustavogustav54@gmail.com>
2026-08-31 11:24:45 -0300

    chore: adjust autocomplete

--- config/nvim/lua/plugins/blink.config.lua
+++ config/nvim/lua/plugins/blink.config.lua
@@ -1,5 +1,9 @@
 return {
   'saghen/blink.cmp',
+  dependencies = {
+    { 'nvim-mini/mini.icons', version = false },
+    "onsails/lspkind.nvim",
+  },
   version = '1.*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -25,7 +29,44 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = {
+      documentation = { auto_show = false, window = { border = 'solid' } },
+      menu = {
+        border = 'none',
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                if ctx.source_name ~= "Path" then
+                  return require("lspkind").symbol_map[ctx.kind] or "" .. ctx.icon_gap
+                end
+
+                local is_unknown_type = vim.tbl_contains({ "link", "socket", "fifo", "char", "block", "unknown" },
+                  ctx.item.data.type)
+                local mini_icon, _ = require("mini.icons").get(
+                  is_unknown_type and "os" or ctx.item.data.type,
+                  is_unknown_type and "" or ctx.label
+                )
+
+                return (mini_icon or ctx.kind_icon) .. ctx.icon_gap
+              end,
+
+              highlight = function(ctx)
+                if ctx.source_name ~= "Path" then return ctx.kind_hl end
+
+                local is_unknown_type = vim.tbl_contains({ "link", "socket", "fifo", "char", "block", "unknown" },
+                  ctx.item.data.type)
+                local mini_icon, mini_hl = require("mini.icons").get(
+                  is_unknown_type and "os" or ctx.item.data.type,
+                  is_unknown_type and "" or ctx.label
+                )
+                return mini_icon ~= nil and mini_hl or ctx.kind_hl
+              end,
+            }
+          }
+        }
+      }
+    },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
--- config/nvim/lua/plugins/kanagawa.config.lua
+++ config/nvim/lua/plugins/kanagawa.config.lua
@@ -29,32 +29,43 @@ return {
           return {}
         end
 
+        local theme = colors.theme
+        local makeDiagnosticColor = function(color)
+          local c = require("kanagawa.lib.color")
+          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+        end
+
         return {
-          Constant = { fg = colors.palette.fujiWhite },
+          Constant                         = { fg = colors.palette.fujiWhite },
 
-          StatusLine = { bg = colors.palette.sumiInk4 },
-          StatusLineNC = { bg = colors.palette.sumiInk2 },
-          ColorColumn = { bg = colors.palette.sumiInk2 },
+          StatusLine                       = { bg = colors.palette.sumiInk4 },
+          StatusLineNC                     = { bg = colors.palette.sumiInk2 },
+          ColorColumn                      = { bg = colors.palette.sumiInk2 },
 
-          TelescopeTitle = { fg = colors.theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = colors.theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = colors.theme.ui.bg_p1, bg = colors.theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = colors.theme.ui.fg_dim, bg = colors.theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = colors.theme.ui.bg_m1, bg = colors.theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = colors.theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = colors.theme.ui.bg_dim, fg = colors.theme.ui.bg_dim },
+          TelescopeTitle                   = { fg = colors.theme.ui.special, bold = true },
+          TelescopePromptNormal            = { bg = colors.theme.ui.bg_p1 },
+          TelescopePromptBorder            = { fg = colors.theme.ui.bg_p1, bg = colors.theme.ui.bg_p1 },
+          TelescopeResultsNormal           = { fg = colors.theme.ui.fg_dim, bg = colors.theme.ui.bg_m1 },
+          TelescopeResultsBorder           = { fg = colors.theme.ui.bg_m1, bg = colors.theme.ui.bg_m1 },
+          TelescopePreviewNormal           = { bg = colors.theme.ui.bg_dim },
+          TelescopePreviewBorder           = { bg = colors.theme.ui.bg_dim, fg = colors.theme.ui.bg_dim },
 
-          Pmenu = { fg = colors.theme.ui.shade0, bg = colors.theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-          PmenuSel = { fg = "NONE", bg = colors.theme.ui.bg_p2 },
-          PmenuSbar = { bg = colors.theme.ui.bg_m1 },
-          PmenuThumb = { bg = colors.theme.ui.bg_p2 },
+          DiagnosticVirtualTextHint        = makeDiagnosticColor(theme.diag.hint),
+          DiagnosticVirtualTextInfo        = makeDiagnosticColor(theme.diag.info),
+          DiagnosticVirtualTextWarn        = makeDiagnosticColor(theme.diag.warning),
+          DiagnosticVirtualTextError       = makeDiagnosticColor(theme.diag.error),
 
-          ["@type"] = { italic = false, bold = true },
-          ["@tag"] = { italic = false },
-          ["@tag.delimiter"] = { fg = colors.palette.sumiInk6 },
-          ["@tag.attribute"] = { italic = true },
-          ["@punctuation.bracket"] = { fg = colors.palette.sumiInk6 },
-          ["@conditional.ternary"] = { fg = colors.palette.oniViolet, italic = false },
+          -- Pmenu                            = { fg = colors.theme.ui.shade0, bg = colors.theme.ui.bg_p1 },
+          -- PmenuSel                         = { fg = "NONE", bg = colors.theme.ui.bg_p2 },
+          -- PmenuSbar                        = { bg = colors.theme.ui.bg_m1 },
+          -- PmenuThumb                       = { bg = colors.theme.ui.bg_p2 },
+
+          ["@type"]                        = { italic = false, bold = true },
+          ["@tag"]                         = { italic = false },
+          ["@tag.delimiter"]               = { fg = colors.palette.sumiInk6 },
+          ["@tag.attribute"]               = { italic = true },
+          ["@punctuation.bracket"]         = { fg = colors.palette.sumiInk6 },
+          ["@conditional.ternary"]         = { fg = colors.palette.oniViolet, italic = false },
           ["@string.documentation.python"] = { link = "Comment" },
         }
       end,
