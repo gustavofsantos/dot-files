132217b69e7eb5342a271b6ef44f94ce9fa7eb6b
Gustavo Santos <gustavogustav54@gmail.com>
2026-09-11 15:22:04 -0300

    chore: update config

---
+++ config/nvim/lua/plugins/colorschemes.config.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "loctvl842/monokai-pro.nvim",
+    tag = "v2.1.4",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("monokai-pro").setup()
+    end,
+  }
+}
